ce37dcd343c58f0941cd669619febaddf7f90f95
Michal ŠMAHEL <admin@ceskydj.cz>
2019-11-03 18:40:34 +0100

    Zakladni struktura

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Index
+ *
+ * Soubor, do něhož míří veškerý provoz, který potřebuje být zpracován aplikací
+ *
+ * @author Michal Šmahel (ceskyDJ) <admin@ceskydj.cz>
+ */
