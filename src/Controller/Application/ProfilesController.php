b7eed8a47a5dab98a582a104582ede1d1670176e
Michal ŠMAHEL <48548230+ceskyDJ@users.noreply.github.com>
2020-05-12 20:48:55 +0200

    Otevirani a zavirani mobilni verze hlavniho menu

---
+++ src/Controller/Application/ProfilesController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller\Application;
+
+/**
+ * Class ProfilesController
+ *
+ * @author Michal Šmahel (ceskyDJ) <admin@ceskydj.cz>
+ * @package App\Controller\Application
+ */
+class ProfilesController
+{
+
+}
--- www/js/classes/HeaderController.js
+++ www/js/classes/HeaderController.js
@@ -2,12 +2,20 @@ class HeaderController
 {
     constructor()
     {
+        this.saveMenuHeight();
+
         this.handleShowNotifications();
+        this.handleOpenCloseMenu();
     }
 
     handleShowNotifications()
     {
         document.querySelector("#_show-notifications-b").addEventListener("click", _ => this.showNotifications());
+    }
+
+    handleOpenCloseMenu()
+    {
+        document.querySelector("#_main-menu-control-b").addEventListener("click", _ => this.openCloseMenu());
     }
 
     showNotifications()
@@ -32,5 +40,58 @@ class HeaderController
 
             box.dataset.open = "true";
         }
+    }
+
+    openCloseMenu()
+    {
+        const menu = document.querySelector("#_main-menu-container");
+        let cycle;
+
+        if(menu.classList.contains("mobile-hide")) {
+            // Open
+            menu.style.height = "0";
+            menu.style.overflowY = "hidden";
+            menu.classList.remove("mobile-hide");
+
+            const openMenu = _ => {
+                let actualHeight = parseInt(menu.style.height.replace("px", ""));
+                menu.style.height = (actualHeight += 4).toString() + "px";
+
+                if(actualHeight >= menu.dataset.height) {
+                    clearInterval(cycle);
+                }
+            }
+
+            cycle = setInterval(openMenu, 10);
+        } else {
+            // Close
+            menu.style.height = menu.dataset.height + "px";
+            menu.style.overflowY = "hidden";
+
+            const closeMenu = _ => {
+                let actualHeight = parseInt(menu.style.height.replace("px", ""));
+                menu.style.height = (actualHeight -= 4).toString() + "px";
+
+                if(actualHeight <= 0) {
+                    clearInterval(cycle);
+                    menu.classList.add("mobile-hide");
+                }
+            }
+
+            cycle = setInterval(closeMenu, 10);
+        }
+    }
+
+    saveMenuHeight()
+    {
+        const menu = document.querySelector("#_main-menu-container");
+
+        menu.style.visibility = "hidden";
+        menu.style.display = "initial";
+
+        menu.dataset.height = menu.scrollHeight.toString();
+
+        menu.style.removeProperty("display");
+        menu.style.removeProperty("visibility")
     }
 }
